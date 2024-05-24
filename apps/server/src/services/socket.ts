dbd1f8b948713f478aec0d0c7e3782be1fefcde1
RITAP DEY <ritapdey@RITAPs-MacBook-Air.local>
2024-05-25 04:28:41 +0530

    first commit

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,31 @@
+import { Server } from "socket.io";
+
+class SocketService {
+    private _io: Server;
+    constructor() {
+        console.log("Init Socket Services...");
+        this._io = new Server({
+            cors: {
+                allowedHeaders: ['*'],
+                origin: '*',
+            },
+        });
+    };
+
+    public initListeners() {
+        const io = this.io;
+        console.log('Init Socket Listeners...');
+        io.on('connect', (socket) => {
+            console.log(`New Socket connected`, socket.id);
+            socket.on('event:message', async ({message}: {message: string}) => {
+                console.log('New Message received', message);
+            });
+        });
+    };
+
+    get io() {
+        return this._io;
+    };
+};
+
+export default SocketService;
